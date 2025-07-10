8dd310f7e8b192bc17e584cfe9dcd6c8bdadbf47
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-10 12:49:34 +0100

    Set the content type of the server response apropriately.

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-
 const app = express();
 const port = 7070;
 const listen_url = 'http://localhost:' + port;
 const api_key = 'your api key here!'
 const get_url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + api_key;
+
 
 let corsConfig = {
     origin: listen_url
@@ -16,11 +16,14 @@ app.get('/', cors(corsConfig), (req, res) => {
     axios.get(get_url)
         .then(
             response => (
+                res.set("Content-Type", "application/json"),
                 res.set("Access-Control-Allow-Origin", "*"),
                 res.send(response.data.results),
                 console.log('Request completed with success.')
             ),
             error => {
+                res.set("Content-Type", "application/json"),
+                res.set("Access-Control-Allow-Origin", "*"),
                 res.send(error),
                 console.log('Request failed with error.\n' + error)
             }
