2dc9f9f977bf3ebb907f8d996ffd6ca969aa4fef
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-10 20:46:51 +0100

    The port and api key configuration should now be stored in a config.json file. An example config file was added.

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
+import {readFile} from 'fs/promises';
+
+const config = JSON.parse(
+    await readFile(new URL('./config.json', import.meta.url))
+);
+
 const app = express();
-const port = 7070;
-const listen_url = 'http://localhost:' + port;
-const api_key = 'your api key here!'
-const get_url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + api_key;
+const listen_url = 'http://localhost:' + config.port;
+const get_url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + config.token;
 
 
 let corsConfig = {
@@ -28,7 +32,7 @@ app.get('/', cors(corsConfig), (req, res) => {
         )
 });
 
-let server = app.listen(port, function() {
+let server = app.listen(config.port, function() {
     let port = server.address().port;
     console.log('Server listening at port %s.', port);
 });
